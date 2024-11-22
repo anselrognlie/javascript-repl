f34187c138bb8185b02ab44e4e9b022c9ad15c18
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-11-22 10:55:41 -0800

    makes a significant change to index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-// Use this file as a general scratch pad for running code.
+// replaced all of this code with my own code
 
-// Run in the terminal from the project directory with
-//   node src/index.js
-
-// Or as
-//   npm start
-
-// Files other than src/index.js must be run using
-//   node path/to/file.js
-
-// Where `path/to/file.js` represents the path to the file to be run relative to
-// the current working directory.
-
-// The `Code Runner` VS Code Extension can be installed to add a Play button for
-// JavaScript code.
-
-// Refer to math.js and math.test.js for an example of a small test.
-// More details will be covered in Unit 3, Tests.
-
-// Run from the terminal with
-//   npm test
-
-// The `Jest` VS Code Extension can be installed so that the tests are detected
-// and appear under the VS Code Testing panel.
-
-// Replace with the code you'd like to run
-console.log('Hello, World!');
-console.log('Goodbye, World!');
+const { sum } = require('./math');
+console.log(sum(1, 2));
